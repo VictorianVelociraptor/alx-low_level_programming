dc09c21e708a6494fa3293d12b6947a4eb5c7ec2
VictorianVelociraptor <helina0ty@gmail.com>
2023-09-08 21:43:08 +0000

    Variables,if,else,while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char h;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (h = 'a'; h <= 'f'; h++)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}
